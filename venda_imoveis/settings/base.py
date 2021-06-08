27cfdf223a381834c6b48f09c458faa8da0b8fcd
felipe frança <felipevictor67@hotmail.com>
2021-06-08 09:38:36 -0300

    alterações static

--- venda_imoveis/settings/base.py
+++ venda_imoveis/settings/base.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -183,6 +182,3 @@ USE_THOUSAND_SEPARATOR=False
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
