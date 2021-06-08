e60cbdde978f65c63b51525ef9a7ec56e6a530b0
felipe frança <felipevictor67@hotmail.com>
2021-06-08 10:55:08 -0300

    correcao dependencias

--- venda_imoveis/settings/base.py
+++ venda_imoveis/settings/base.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -182,3 +183,6 @@ USE_THOUSAND_SEPARATOR=False
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
